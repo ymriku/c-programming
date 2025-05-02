0426ee96249c05cdea807cc6308adbd104c2091e
ymriku <rmp-150-wgn@e-net.nara.jp>
2025-05-02 05:25:35 +0000

    5_2

--- 5_2/10.c
+++ 5_2/10.c
@@ -1,8 +1,10 @@
-#include <stdio.h>
+#include<stdio.h>
 
-// ここに関数を定義しよう
-
-int main() {
-    // 関数を呼び出そう
+int add(int a, int b) {
+    return a + b;
+}
+int main(void){
+    int result = add(3, 5); 
+    printf("3 + 5 = %d\n", result);
     return 0;
 }
