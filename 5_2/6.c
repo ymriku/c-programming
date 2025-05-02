00a70f1ffde26f91a974c261ed266c4df55ecad8
ymriku <rmp-150-wgn@e-net.nara.jp>
2025-05-02 05:42:10 +0000

    5_2

--- 5_2/6.c
+++ 5_2/6.c
@@ -3,7 +3,7 @@
 int main()   {
     int i,n
    scanf("%d",&n);
-    for(int i=1;i<=n;i++){ // for文で1から10までを出力しよう
+    for(int i=1;i<=n;i++){ // for文で1からnまでを出力しよう
     printf("%d\t",i);
    } 
     return 0;
